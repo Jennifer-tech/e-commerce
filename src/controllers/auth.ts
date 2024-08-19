49c2b5db0c60653b40417af79aae8c8a61bf3512
Jennifer-tech <muofunanyajennifer1@gmail.com>
2024-08-19 17:24:32 +0100

    Feat: Added error handling

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,17 +1,20 @@
-import {NextFunction, Request, Response} from 'express'
+import { NextFunction, Request, Response } from 'express'
 import { prismaClient } from '..';
-import {hashSync, compareSync} from 'bcrypt'
+import { hashSync, compareSync } from 'bcrypt'
 import * as jwt from 'jsonwebtoken'
 import { JWT_SECRET } from '../secrets';
 import { BadRequestException } from '../exceptions/bad-requests';
 import { ErrorCode } from '../exceptions/root';
+import { UnprocessableEntity } from '../exceptions/validations';
+import { SignUpSchema } from '../schema/users';
 
 export const signup = async (req: Request, res: Response, next: NextFunction) => {
-    const { email, password, name} = req.body;
+    SignUpSchema.parse(req.body)
+    const { email, password, name } = req.body;
 
-    let user = await prismaClient.user.findFirst({where: {email}})
+    let user = await prismaClient.user.findFirst({ where: { email } })
 
-    if(user) {
+    if (user) {
         next(new BadRequestException('User already exist', ErrorCode.USER_ALREADY_EXISTS))
     }
     user = await prismaClient.user.create({
@@ -25,14 +28,14 @@ export const signup = async (req: Request, res: Response, next: NextFunction) =>
 }
 
 export const login = async (req: Request, res: Response) => {
-    const { email, password} = req.body;
+    const { email, password } = req.body;
 
-    let user = await prismaClient.user.findFirst({where: {email}})
+    let user = await prismaClient.user.findFirst({ where: { email } })
 
-    if(!user) {
+    if (!user) {
         throw Error("User does not exists!")
     }
-    if(!compareSync(password, user.password)) {
+    if (!compareSync(password, user.password)) {
         throw Error('Incorrect password!')
     }
 
@@ -40,5 +43,5 @@ export const login = async (req: Request, res: Response) => {
         userId: user.id
     }, JWT_SECRET)
 
-    res.json({user, token})
+    res.json({ user, token })
 }
--- src/exceptions/root.ts
+++ src/exceptions/root.ts
@@ -1,3 +1,5 @@
+import { UnprocessableEntity } from "./validations";
+
 export class HttpException extends Error {
     message: string;
     errorCode: any;
@@ -16,5 +18,7 @@ export class HttpException extends Error {
 export enum ErrorCode {
     USER_NOT_FOUND = 1001,
     USER_ALREADY_EXISTS = 1002,
-    INCORRECT_PASSWORD = 1003
+    INCORRECT_PASSWORD = 1003,
+    UNPROCESSABLE_ENTITY = 2001,
+    INTERNAL_EXCEPTION = 3001
 }
